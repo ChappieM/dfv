6baa10f44e02272f984802aba4de47c88fa334f0
chappiem <cha39881@gmail.com>
2023-06-08 03:44:54 +0900

    feat: subprocess example

--- src/main.py
+++ src/main.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 
-print("welcome")
+
+import subprocess
+
+command = ["ls", "-l"]
+up = ["devcontainer", "up", "--workspace-folder",  "."]
+
+res = subprocess.run(up, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+print(res.stdout.decode())
+
+if __name__=="__main__":
+    print("asdf")
