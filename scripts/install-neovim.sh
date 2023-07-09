d8ca590225d2ff2f3c6888cf5869b422afd55475
chappiem <cha39881@gmail.com>
2023-07-10 03:21:16 +0900

    fix: wIP

--- scripts/install-neovim.sh
+++ scripts/install-neovim.sh
@@ -1,17 +1,7 @@
 #! /bin/bash
 
-WORK_DIR='/var/tmp'
-NVIM_APP_DIR='~/my-apps/nvim'
-
-cd ${WORK_DIR}
-
-# install neovim
-ehoc "Install neovim"
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-rm nvim.appimage
-
-mkdir -p ${NVIM_APP_DIR}
-mv squashfs-root ${NVIM_APP_DIR}
-ln -s ${NVIM_APP_DIR}/AppRun /usr/bin/nvim
+cd /tmp
+git clone https://github.com/neovim/neovim
+git checkout stable
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
